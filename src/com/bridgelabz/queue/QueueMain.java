01abf48414c46981650c5c39f285ba987d8bd848
hansani111 <hansanideshmukh894@gmail.com>
2023-01-21 17:04:01 +0530

    added: dequeue method

--- src/com/bridgelabz/queue/QueueMain.java
+++ src/com/bridgelabz/queue/QueueMain.java
@@ -5,11 +5,12 @@ public class QueueMain {
     public static void main(String[] args) {
 
         Queue<Integer> queue1 = new Queue<>();
-
         queue1.enqueue(56);
         queue1.enqueue(30);
         queue1.enqueue(70);
 
+        queue1.display();
+        queue1.dequeue();
         queue1.display();
     }
 }
