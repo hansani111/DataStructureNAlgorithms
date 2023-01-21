2672857b6d0c1cccdfee0f2c2f004183720311d6
hansani111 <hansanideshmukh894@gmail.com>
2023-01-21 12:55:20 +0530

    added: create multiple nodes

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -6,5 +6,10 @@ public class LinkedListMain {
         LinkedList<Integer> Linkedlist1 = new LinkedList<>();
         Linkedlist1.push(70);
 
+        Linkedlist1.push(56);
+        Linkedlist1.push(30);
+
+        Linkedlist1.display();
+
     }
 }
